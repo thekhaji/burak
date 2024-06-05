b5f5b0211cf304b4cbef1ad131be19a9fdcf8928
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-06-06 00:59:18 +0900

    feat: solution of H-TASK

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+console.log("H-TASK");
+
+function getPositive(arrNum:number[]){
+    let posNums:string = "";
+    arrNum.map((e)=>{
+        if(e>=0)
+            posNums+=e;
+    });
+
+    return posNums;
+}
+
+console.log(getPositive([1, -4, 2]));
