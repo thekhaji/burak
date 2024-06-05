69518ce6d82616027d92a35b16185517a43681d3
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-06-06 00:20:32 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
+
+import mongoose from 'mongoose';
+mongoose.connect(process.env.MONGO_URL as string, {})
+    .then((data)=>{
+        console.log("MongoDB connection succeed");
+        const PORT = process.env.PORT ?? 3077;        
+    })
+    .catch((err)=>{
+        console.log("ERROR on connection MongoDB", err);
+        
+    });
 
 /*Architectural pattern: MVC(Model View Controller). DI(Dependency Injection), MVP(Mode View Presenter)
     Backend dagi ma'lumotlar oqimini tartibga soladigon bir arxitektura/vositachi
