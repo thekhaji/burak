186815ea42f1c72996966b4e4a95a44746372720
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-06-05 23:12:27 +0900

    feat: integration off enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+/*Architectural pattern: MVC(Model View Controller). DI(Dependency Injection), MVP(Mode View Presenter)
+    Backend dagi ma'lumotlar oqimini tartibga soladigon bir arxitektura/vositachi
+    Suyagi
+*/
+
+
+/*Design patter: Middleware, Decorator(PIPE, GUARDD,)
+    Ma'lum bir bolaklarni strukturasini yechishda hizmat qiladigon pattern
+    Qo'lining suyagi
+*/
