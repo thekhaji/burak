76a2b5a0edf0613c9c174a586ce2df6358bad386
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-06-20 21:29:09 +0900

    fix: solution MIT TASK-K

--- src/train.ts
+++ src/train.ts
@@ -79,3 +79,17 @@
 /*
     cookies joins each requests
  */
+
+
+function countVowels(word: String){
+    const vowels = "aeiouAEIOU";
+    let count = 0;
+    for(let i=0; i<word.length; i++){
+        if(vowels.includes(word[i]))
+            count++;
+    }
+    return count;
+
+}
+
+console.log(countVowels("Xojiakbar"));
