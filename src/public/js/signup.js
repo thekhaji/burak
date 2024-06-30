b5b20c6d8a333c9f9b9aba0b81de102de71eed80
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-06-30 18:09:16 +0900

    feat: frontend validation in signup page

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,29 @@
 console.log("Signup frontend javascript file");
 
 $(function(){
+    const fileTarget = $(".file-box .upload-hidden");
+    let fileName;
+
+    fileTarget.on("change", function(){
+        if(window.FileReader){
+            const uploadFile = $(this)[0].files[0] ;
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if (!validImageType.includes(fileType)){
+                alert("Please insert only jpeg, jpg and png formats!");
+            }
+            else{
+                if(uploadFile){
+                    $("upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("success");
+                }
+                fileName = $(this)[0].files[0].name ;
+            }
+            $(this).siblings(".upload-name").val(fileName);
+        }   
+
+    }) ;
 
 });
 
@@ -21,5 +44,14 @@ function validateSignupForm(){
         return false;  
     }
 
-   
+   const memberImage = $(".member-image").get(0).files[0]
+        ? $(".member-image").get(0).files[0].name 
+        : null;
+
+    if(!memberImage){
+        alert("Please insert restaurant image!")
+        return false;
+    }
+
+
 }
