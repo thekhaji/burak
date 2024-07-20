75bd4d7920c609ee6e7d547ae1955cb97e5e0c60
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-21 08:22:53 +0900

    fix: modify getMemberetail business logic

--- src/controllers/member.contoroller.ts
+++ src/controllers/member.contoroller.ts
@@ -55,7 +55,20 @@ memberController.logout = (req: ExtendedRequest, res: Response) => {
         res.status(HttpCode.OK).json({logout: true});
     }
     catch(err){
-        console.log("Error,Logout:", err);
+        console.log("Error, Logout:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);   
+    }
+}
+
+memberController.getMemberDetail = async (req: ExtendedRequest, res: Response) => {
+    try{
+        console.log("getMemberDetail");
+        const result = await memberService.getMemberDetail(req.member);
+        res.status(HttpCode.OK).json(result);
+    }
+    catch(err){
+        console.log("Error, getMemberDetail:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);   
     }
