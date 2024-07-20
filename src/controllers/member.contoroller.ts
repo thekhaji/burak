e4a12dd9c9ab522f4fcdbe600085b3f0eb95e332
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-07-21 06:06:58 +0900

    feat: develop authentication via tokens with cookies

--- src/controllers/member.contoroller.ts
+++ src/controllers/member.contoroller.ts
@@ -3,8 +3,9 @@ import {T} from "../libs/types/common"
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import MemberService from "../models/Member.service";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 const memberService = new MemberService();
 const authService = new AuthService();
@@ -18,11 +19,9 @@ memberController.signup = async (req: Request, res: Response)=>{
         const input: MemberInput = req.body,
         result: Member = await memberService.signup(input);
         const token = await authService.createToken(result);
+        res.cookie("accessToken", token, {maxAge: AUTH_TIMER * 3600 * 1000, httpOnly: false});
 
-        console.log("token => ",token);
-
-        // TODO:TOKENS
-        res.json({member: result});
+        res.status(HttpCode.CREATED).json({member: result, accessToken: token});
     }
     catch(err){
         console.log("Error, signup:", err);
@@ -37,14 +36,9 @@ memberController.login = async (req: Request, res: Response)=>{
         const input: LoginInput = req.body,
         result = await memberService.login(input),
         token = await authService.createToken(result);
+        res.cookie("accessToken", token, {maxAge: AUTH_TIMER * 3600 * 1000, httpOnly: false});
 
-        console.log("token => ",token);
-        
-         // TODO:TOKENS
-
-
-
-        res.json({member: result});
+        res.status(HttpCode.OK).json({member: result, accessToken: token});
     }
     catch(err){
         console.log("Error,processLogin:", err);
